9ac65cb3bf711b1fbb70cbede6594b423c5531d9
vina-cpu <victorianawalany@gmail.com>
2025-04-22 22:51:07 -0600

    mocking email services

--- tests/test_services/test_user_service.py
+++ tests/test_services/test_user_service.py
@@ -1,14 +1,20 @@
 from builtins import range
 import pytest
+from unittest import mock
 from sqlalchemy import select
 from app.dependencies import get_settings
 from app.models.user_model import User
-from app.services.user_service import UserService
+from app.services.user_service import UserService, EmailService
 
 pytestmark = pytest.mark.asyncio
 
+@pytest.fixture
+def mock_send_email(mocker):
+    mock = mocker.patch.object(EmailService, 'send_verification_email', return_value=None)
+    return mock
+
 # Test creating a user with valid data
-async def test_create_user_with_valid_data(db_session, email_service):
+async def test_create_user_with_valid_data(db_session, email_service, mock_send_email):
     user_data = {
         "email": "valid_user@example.com",
         "password": "ValidPassword123!",
@@ -16,6 +22,7 @@ async def test_create_user_with_valid_data(db_session, email_service):
     user = await UserService.create(db_session, user_data, email_service)
     assert user is not None
     assert user.email == user_data["email"]
+    mock_send_email.assert_called_once()
 
 # Test creating a user with invalid data
 async def test_create_user_with_invalid_data(db_session, email_service):
@@ -90,7 +97,7 @@ async def test_list_users_with_pagination(db_session, users_with_same_role_50_us
     assert users_page_1[0].id != users_page_2[0].id
 
 # Test registering a user with valid data
-async def test_register_user_with_valid_data(db_session, email_service):
+async def test_register_user_with_valid_data(db_session, email_service, mock_send_email):
     user_data = {
         "email": "register_valid_user@example.com",
         "password": "RegisterValid123!",
@@ -98,6 +105,7 @@ async def test_register_user_with_valid_data(db_session, email_service):
     user = await UserService.register_user(db_session, user_data, email_service)
     assert user is not None
     assert user.email == user_data["email"]
+    mock_send_email.assert_called_once()
 
 # Test attempting to register a user with invalid data
 async def test_register_user_with_invalid_data(db_session, email_service):
