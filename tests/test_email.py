f185f12790611f1df6474a74cc8b4a76f0a3f049
vina-cpu <victorianawalany@gmail.com>
2025-04-23 20:47:43 -0600

    using mocking for email

--- tests/test_email.py
+++ tests/test_email.py
@@ -1,8 +1,9 @@
 import pytest
+from unittest import mock
 from app.services.email_service import EmailService
 from app.utils.template_manager import TemplateManager
 
-    
+# Mailtrap in 11 ?
 @pytest.mark.asyncio
 async def test_send_markdown_email(email_service):
     user_data = {
@@ -10,5 +11,8 @@ async def test_send_markdown_email(email_service):
         "name": "Test User",
         "verification_url": "http://example.com/verify?token=abc123"
     }
-    await email_service.send_user_email(user_data, 'email_verification')
-    # Manual verification in Mailtrap
+    with mock.patch.object(email_service, 'send_user_email') as mock_send_email:
+        await email_service.send_user_email(user_data, 'email_verification')
+        
+        mock_send_email.asser_called_once_with(user_data, 'email_verification')
+        
